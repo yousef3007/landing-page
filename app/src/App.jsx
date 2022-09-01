7dac6d20fe2107041e6bd1d3878b40bb636a993a
Yousef <gegsbhxhx@gmail.com>
2022-09-01 13:09:35 +0200

    finish

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,7 +1,31 @@
+import About from "./components/about";
+import Contact from "./components/contact";
+import Portofolio from "./components/portofolio";
+import Header from "./components/header";
+const list = ["Home","Services","About","Sign Up"];
+function NavBar(){
+  return(
+    <nav className="navbar">
+      <div className="navbar-container">
+        <a href="/" id="myLink">Home</a>
+        <div id="nav">
+            {list.map(key => {
+            return(
+              <a href={`#${key}`} className="item">{key}</a>
+            )
+          })}
+        </div>
+      </div>
+    </nav>    
+  )
+}
 function App() {
   return (
     <div>
-      Hello world
+      <Header />
+      <About />
+      <Portofolio />
+      <Contact />
     </div>
   )
 }
